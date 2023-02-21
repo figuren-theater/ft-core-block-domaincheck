61843f9a8df979cac26651dff47d13da475a6523
carstenbach <mail@carsten-bach.de>
2023-02-21 01:56:14 +0100

    BUILD

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '34f5921c4558ea5c643d1ad6ca625ffb');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '206c6e9f5f25246c0a38');
